f630c6ab961cf63951f9f5cf714f4dd403fbdee3
liseami <liseami@qq.com>
2021-10-19 10:45:09 +0800

    go

--- Motion/AppDelegate.swift
+++ Motion/AppDelegate.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import MotionComponents
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         AppearConfig.config()
