16e78d7414f470a8036dfc7008d280292df91f29
liseami <liseami@qq.com>
2021-10-22 16:42:03 +0800

    pod

--- Motion/AppDelegate.swift
+++ Motion/AppDelegate.swift
@@ -12,9 +12,7 @@ import MotionComponents
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         AppearConfig.config()
-        
         UMRUN()
-        
         return true
     }
     
